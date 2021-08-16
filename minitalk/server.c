2cfaf3efb32088e3b4c1ad65e076a38e1365002b
Manuel Rubio <manuel.rubio.arias@gmail.com>
2021-08-16 18:22:13 +0200

    kk

--- minitalk/server.c
+++ minitalk/server.c
@@ -6,18 +6,37 @@
 /*   By: mrubio <mrubio@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/05 16:05:14 by mrubio            #+#    #+#             */
-/*   Updated: 2021/08/10 23:45:40 by mrubio           ###   ########.fr       */
+/*   Updated: 2021/08/11 00:06:53 by mrubio           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-static void get_bit(int bit)
+void	get_char_from_bits(int sigusr)
 {
-	if (bit == SIGUSR1)
-		write(1, "1", 1);
-	else if (bit == SIGUSR2)
-		write(1, "0", 1);
+	static char	c = 0;
+	static int	i = 8;
+
+	if (sigusr == SIGUSR1)
+		c |= (0 << --i);
+	else if (sigusr == SIGUSR2)
+		c |= (1 << --i);
+	if (i == 0)
+	{
+		if (c == '\0')
+			write(1, "\n", 1);
+		write(1, &c, 1);
+		i = 8;
+		c = 0;
+	}
+}
+
+void	get_sig(int sigusr)
+{
+	if (sigusr == SIGUSR1 || sigusr == SIGUSR2)
+		get_char_from_bits(sigusr);
+	else
+		exit(1);
 }
 
 int main()
@@ -27,12 +46,10 @@ int main()
 	process_id = getpid();
 	printf("Process ID (PID): %d\n", process_id);
 	printf("\n...Waiting to receive data...\n\n");
+	signal(SIGUSR1, get_sig);
+	signal(SIGUSR2, get_sig);
 	while(1)
-	{
-		signal(SIGUSR1, get_bit);
-		signal(SIGUSR2, get_bit);
 		pause();
-	}
 	return 0;
 }
 
